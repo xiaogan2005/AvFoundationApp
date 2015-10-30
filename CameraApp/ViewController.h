946b8b592da76805eb7e4807098785ee857c6586
xiaogan2005 <xiaogan2005@gmail.com>
2015-10-30 09:46:10 -0400

    Initial Commit

---
+++ CameraApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CameraApp
+//
+//  Created by GuoRui on 10/30/15.
+//  Copyright © 2015 GuoRui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
